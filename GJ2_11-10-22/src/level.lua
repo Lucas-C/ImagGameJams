fce524e1d44ab0b0768e943805c613b678741a91
dureuill <louis.dureuil@ensimag.imag.fr>
2011-10-23 14:46:24 +0200

    * Ajout de obstacle.lua * level:importLevel terminé (aux tests près...) * légères modifications internes de line.lua

--- GJ2_11-10-22/src/level.lua
+++ GJ2_11-10-22/src/level.lua
@@ -3,9 +3,32 @@ function level:addLine(line)
 end
 
 function level:importLevel(filename)
+	level = {}
 	if (love.filesystem.isFile(filename)) then
 		exit(3)
 	end
-	
+	contents = love.filesystem.read(filename)
+	local ln = contents:sub(1, 1)
+	local d = 0
+	local cl = 0
+	for i = 2, #contents do
+		local c = contents:sub(i, i)
+		if c == "\n" then
+			d = i
+			break
+		end
+	end
+	local sums = {}
+	for i = d+1, #contents do
+		local c = contents:sub(i, i)
+		if c == "\n" then
+			cl = (cl + 1) % ln
+		else 
+			sums[cl+1] = sums[cl+1] + 1
+			if c == "h" then
+				obstacle:getNew("h", sums[cl+1])
+				level[cl+1]:addObstacle(obstacle)
+		end
+	end	
 end
 
