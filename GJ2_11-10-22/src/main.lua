4ca80df9b6ecd62b675b98968ebc37af564a66a9
unknown <gloups@.(none)>
2011-10-23 12:01:19 +0200

    camera correction

--- GJ2_11-10-22/src/main.lua
+++ GJ2_11-10-22/src/main.lua
@@ -3,13 +3,14 @@ require("camera")
 
 camera.x = 0
 camera.y = 0
+speedCamera = 100
 
 function love.draw()
   camera:set()
-  camera.x = camera.x + 1
   love.graphics.print("Hello World", 400, 300)
   camera:unset()
 end
 
 function love.update(dt)
+  camera.x = camera.x + speedCamera * dt
 end
