f2924f8e2eccf3cf39fe3235af57c0596a4691c4
dureuill <louis.dureuil@ensimag.imag.fr>
2011-10-23 13:09:48 +0200

    line.lua

---
+++ GJ2_11-10-22/src/line.lua
@@ -0,0 +1,23 @@
+line = {}
+
+-- Renvoie les obstacles entre min et max.
+function line:getObstacles(min, max)
+index = 0
+obstacles = {}
+for i = min, max do
+if line.i ~= nil then
+obstacles.index = line.i
+index = index + 1
+end
+
+end
+
+end
+
+-- Ajoute l'obstacle à la ligne 
+function line:addObstacle(obstacle)
+end
+
+-- Lit un niveau dans un fichier.
+function line:readLevel(filename)
+end
