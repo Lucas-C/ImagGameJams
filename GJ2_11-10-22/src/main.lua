f0403adc09db0c40e75348c8bef564d35e67d17c
unknown <gloups@.(none)>
2011-10-23 11:40:50 +0200

    Camera module

--- GJ2_11-10-22/src/main.lua
+++ GJ2_11-10-22/src/main.lua
@@ -1,3 +1,15 @@
+
+require("camera")
+
+camera.x = 0
+camera.y = 0
+
 function love.draw()
-    love.graphics.print("Hello World", 400, 300)
+  camera:set()
+  camera.x = camera.x + 1
+  love.graphics.print("Hello World", 400, 300)
+  camera:unset()
+end
+
+function love.update(dt)
 end
