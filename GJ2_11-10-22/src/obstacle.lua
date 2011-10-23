5ab911d398b924fb37c1831b664ffb364c7b9c0e
unknown <Juleffel@.(none)>
2011-10-23 16:34:47 +0200

    Gestion des differents obstacles dans obstacle

--- GJ2_11-10-22/src/obstacle.lua
+++ GJ2_11-10-22/src/obstacle.lua
@@ -2,5 +2,9 @@ function getNewObstacle(oType, position)
 	obstacle = {}
 	obstacle.oType = oType
 	obstacle.position = position 
+	obstacle.sprite = nil
+	if obstacle.oType == "h" then
+		obstacle.sprite = love.graphics.newImage("assets/hurdle.png")
+	end
 	return obstacle
 end
