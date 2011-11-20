dc9aace99449d7d55f8f54f72d469319e7c37539
Lucas Cimon <lucas.cimon@ensimag.fr>
2011-11-21 00:03:27 +0100

    final version

--- GJ3_11-11-20/GuillaumeTell007/Assets/Ennemy Assets/Ennemy.js
+++ GJ3_11-11-20/GuillaumeTell007/Assets/Ennemy Assets/Ennemy.js
@@ -1,7 +1,46 @@
-var target : Transform;
+
+var player : Transform;
+
+private var speed : float = 0.02;
+private var deltaAngle : float = 1;
+private var updateCount : int = 0;
+private var rotate : float = 0;
+function FixedUpdate()
+{
+	if (CanSeePlayer())
+	{
+		var angle : float = ((player.eulerAngles - transform.eulerAngles).y + 360) % 360;
+		if (180 - deltaAngle < angle && angle < 180 + deltaAngle)
+			rotate = 0.1;
+		else
+			rotate = 1;
+		
+		rotate = angle < 180 ? -rotate : rotate;
+	}
+	else if (++updateCount == 100)
+	{
+		updateCount = 0;
+		rotate = Random.Range(-0.5, 0.5);
+	}
+
+	transform.Translate(Vector3(0, 0, speed));
+	transform.Rotate(0, rotate, 0);
+}
+
+function CanSeePlayer () : boolean {
+	var playerDirection : Vector3 = (player.position - transform.position);
+	var hit : RaycastHit;
+	Physics.Raycast (transform.position, playerDirection, hit, playerDirection.magnitude);
+	if (hit.collider && hit.collider.transform == player) {
+		return true;
+	}
+	return false;
+}
+
+
+/*** Life ***/
 
 var life : int = 2;
-	
 
 private var dead = false;
 function ApplyDamage(damage:int)
