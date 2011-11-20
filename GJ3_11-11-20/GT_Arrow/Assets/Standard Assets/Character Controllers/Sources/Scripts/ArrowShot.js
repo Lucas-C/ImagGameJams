7e0a9e4114dda945b2263ded351c2b65914b531c
Cyril Kamowski <kamowcyr@ensimag.fr>
2011-11-20 14:32:28 +0100

    Arrow

---
+++ GJ3_11-11-20/GT_Arrow/Assets/Standard Assets/Character Controllers/Sources/Scripts/ArrowShot.js
@@ -0,0 +1,12 @@
+
+var projectile : Rigidbody;
+var speed = 1000;
+function Update()
+{
+	if( Input.GetButtonDown( "Fire1" ) )
+	{
+		var instantiatedProjectile : Rigidbody = Instantiate(projectile, transform.position, transform.rotation);
+		instantiatedProjectile.velocity = transform.TransformDirection( Vector3( 0, 0, speed ) );
+		Physics.IgnoreCollision( instantiatedProjectile.collider, transform.root.collider );
+	}
+}
